b790015d0c371c30da4e9c43483068b1d1ba20ab
n.rusev <n.rusev@tinqin.com>
2021-10-20 10:56:04 +0300

    move to ES2015 import statements

--- server/src/client/components/Home.js
+++ server/src/client/components/Home.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-    return <div>I'm the very very best home component</div>;
+    return (
+        <div>
+            <div>I'm the very very best home component</div>
+            <button onClick={()=>console.log("Hi there!")}>Press me!</button>
+        </div>);
 }
 
 export  default Home;
