19e4f7e0d5785b5feade7b06d32d477fa9bb1666
n.rusev <n.rusev@tinqin.com>
2021-10-20 16:24:00 +0300

    update user list component

--- server/src/client/components/UsersList.js
+++ server/src/client/components/UsersList.js
@@ -6,4 +6,27 @@ class UsersList extends Component {
     componentDidMount() {
         // this.props.fetchUsers();
     }
+
+    renderUsers() {
+        return this.props.users.map(user => {
+            return <li key={user.id}>{user.name}</li>;
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                Here's a big list of users
+                <ul>
+                    {this.renderUsers()}
+                </ul>
+            </div>
+        );
+    }
 }
+
+function mapStateToProps(state) {
+    return {users: state.users};
+}
+
+export default connect(mapStateToProps, {fetchUsers})(UsersList);
