9e840a09b3eddc4b86f40202cc2245b9a9188f9f
n.rusev <n.rusev@tinqin.com>
2021-10-20 11:37:34 +0300

    user hydrate instead of render

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -3,5 +3,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Home from "./components/Home";
 
-ReactDOM.render(<Home />, document.querySelector("#root"));
+ReactDOM.hydrate(<Home />, document.querySelector("#root"));
 
