88f8acf0319bdbc730b1102370c794b204af91bf
n.rusev <n.rusev@tinqin.com>
2021-10-20 16:20:37 +0300

    add user list component

---
+++ server/src/client/components/UsersList.js
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+import {connect} from "react-redux";
+import {fetchUsers} from "../actions";
+
+class UsersList extends Component {
+    componentDidMount() {
+        // this.props.fetchUsers();
+    }
+}
