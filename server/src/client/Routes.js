8339a6228ac4f0d460291ae852906476b9834308
n.rusev <n.rusev@tinqin.com>
2021-10-20 15:11:35 +0300

    update

--- server/src/client/Routes.js
+++ server/src/client/Routes.js
@@ -3,7 +3,9 @@ import {Route} from "react-router-dom";
 import Home from "./components/Home";
 
 export default  () => {
-    return (<div>
-        <Route exact path="/" component={Home} />
-    </div>);
+    return (
+        <div>
+            <Route exact path="/" component={Home} />
+        </div>
+    );
 }
