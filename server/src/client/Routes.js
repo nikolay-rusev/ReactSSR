20b9e2a3edbee2996c095f9b64674068ee1ac2f2
Nikolay Rusev <nikolay.z.rusev@gmail.com>
2021-10-26 18:36:48 +0300

    add not found page

--- server/src/client/Routes.js
+++ server/src/client/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import App from "./App";
 import HomePage from "./pages/HomePage";
 import UsersListPage from "./pages/UsersListPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 // route config for use with react-router-config
 export default [
@@ -16,6 +17,10 @@ export default [
             {
                 ...UsersListPage,
                 path: "/users"
+            },
+            {
+                // show when no match, no path needed
+                ...NotFoundPage
             }
         ]
     }
