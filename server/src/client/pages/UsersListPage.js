f0399db320251884d774694eb2eb607c32022c2b
Nikolay Rusev <nikolay.z.rusev@gmail.com>
2021-10-29 11:29:02 +0300

    add Helmet, set meta tags on client/server

--- server/src/client/pages/UsersListPage.js
+++ server/src/client/pages/UsersListPage.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
 import {fetchUsers} from "../actions";
+import {Helmet} from "react-helmet";
 
 class UsersListPage extends Component {
     componentDidMount() {
@@ -16,6 +17,10 @@ class UsersListPage extends Component {
     render() {
         return (
             <div>
+                <Helmet>
+                    <title>Users App</title>
+                    <meta property="og:title" content="Users App" />
+                </Helmet>
                 Here's a big list of users
                 <ul>
                     {this.renderUsers()}
