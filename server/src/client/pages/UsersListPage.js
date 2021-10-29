1bc488dd90a302395ffc54f8f50ddbe62f5c87d9
Nikolay Rusev <nikolay.z.rusev@gmail.com>
2021-10-29 11:33:13 +0300

    add dynamic data to Helmet

--- server/src/client/pages/UsersListPage.js
+++ server/src/client/pages/UsersListPage.js
@@ -14,13 +14,19 @@ class UsersListPage extends Component {
         })
     }
 
+    head() {
+        return (
+            <Helmet>
+                <title>{`${this.props.users.length} Users Loaded`}</title>
+                <meta property="og:title" content="Users App" />
+            </Helmet>
+        );
+    }
+
     render() {
         return (
             <div>
-                <Helmet>
-                    <title>Users App</title>
-                    <meta property="og:title" content="Users App" />
-                </Helmet>
+                {this.head()}
                 Here's a big list of users
                 <ul>
                     {this.renderUsers()}
